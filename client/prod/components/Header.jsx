7226c289261042b23514ab6175c5ce722b6f6d0e
elgeesound <elgeesound@gmail.com>
2017-11-21 10:20:59 -0800

    update

--- client/prod/components/Header.jsx
+++ client/prod/components/Header.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
-const Header = (props) => {
-  return (
-    <div>
-      Hello Header! I'm gonna take the Profile Component
-    </div>
-  );
-};
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+    }
+  };
+
+  handleTouchTap = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      open: true,
+      anchorEl: e.currentTarget
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <RaisedButton
+          onClick={this.handleTouchTap}
+          label="Account Info"
+        />
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+        >
+          <Menu>
+            <MenuItem primaryText="Trip History" />
+            <MenuItem primaryText="User Profile" />
+            <MenuItem primaryText="Logout" />
+          </Menu>
+        </Popover>
+      </div>
+      )
+  }
+}
 
 export default Header;
