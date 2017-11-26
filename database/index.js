2c26d97b1a80ed07f69cea69cc2b38fafac4fc2c
Jacob <jphill0808@gmail.com>
2017-11-25 19:39:29 -0500

    commit

--- database/index.js
+++ database/index.js
@@ -27,7 +27,7 @@ var Users = new mongoose.Schema({
       price: Number
     })],
     logs: [[]]
-  },
+  }, 
 
   tripsHistory: []
 });
@@ -35,6 +35,12 @@ var Users = new mongoose.Schema({
 
 
 var User = mongoose.model('user', Users);
+
+var sampleUser = {
+  username: 'Mark',
+  email: 'Markus1998@yahoo.com',
+  location: 'Sandy Springs'
+}
 
 let saveUser = ((user, cb) => {
   let newUser = new User({
@@ -56,22 +62,15 @@ let saveUser = ((user, cb) => {
   });
 
   newUser.save((err, data) => {
-    if (err) { cb(err) }
+    if (err) { cb(err) } 
     else { console.log(data) }
   });
 })
 
-
-/*var sampleUser = {
-  username: 'Mark',
-  email: 'Markus1998@yahoo.com',
-  location: 'Sandy Springs'
-}
-
 saveUser(sampleUser, (err, data) => {
   if (err) console.log(err);
 });
-*/
+
 
 module.exports.User = User;
 
